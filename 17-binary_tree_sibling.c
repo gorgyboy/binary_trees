c513576109de5b779911eba9d48a841cbda05b6b
gorgyboy <jlrogb@gmail.com>
2020-11-26 22:01:18 +0000

    Refactor code

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,19 +9,15 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL ||
-	    (node->parent->left == NULL || node->parent->right == NULL))
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
 	if (node->parent->left == node)
-	{
-		if (node->parent->right != NULL)
-			return (node->parent->right);
-	}
+		return (node->parent->right);
+
 	else if (node->parent->right == node)
-	{
-		if (node->parent->left != NULL)
-			return (node->parent->left);
-	}
-	return (NULL);
+		return (node->parent->left);
+
+	else
+		return (NULL);
 }
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
 /**
  * binary_tree_uncle - finds the uncle of a node.
  * @node: Pointer to the node to find the uncle.
@@ -9,18 +11,29 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent, *ancestor;
-
-	parent = node->parent;
-	ancestor = parent->parent;
-
-	if (node == NULL || parent == NULL || ancestor == NULL)
+	if (node == NULL || node->parent == NULL ||
+	    node->parent->parent == NULL)
 		return (NULL);
 
-	if (ancestor->left == parent)
-		return (ancestor->right);
-	else if (ancestor->right == parent)
-		return (ancestor->left);
+	return (binary_tree_sibling(node->parent));
+}
+
+/**
+ * binary_tree_sibling - finds the sibling of a node.
+ * @node: Pointer to the node to find the sibling.
+ *
+ * Return: Pointer to the sibling node. If node is NULL or the parent is NULL,
+ * return NULL. If node has no sibling, return NULL.
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else if (node->parent->right == node)
+		return (node->parent->left);
 	else
 		return (NULL);
 }
