3f2eab58b822342cb7e25297b87ad22f708dd07f
gorgyboy <jlrogb@gmail.com>
2020-11-27 02:29:05 +0000

    Refactor code and improve recursion logic

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,16 +11,18 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int b_left = 0;
 	int b_right = 0;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 		return (0);
 
-	b_left += binary_tree_balance(tree->left);
-	b_right -= binary_tree_balance(tree->right);
-
+	/**
+	 * If a child exists, then the balance counter for that child is equal
+	 * to the accumulated from the child's children plus one from the
+	 * current child, else it would be zero, becuase there is no children.
+	 */
 	if (tree->left != NULL)
-		b_left++;
+		b_left = binary_tree_balance(tree->left) + 1;
 	if (tree->right != NULL)
-		b_right--;
+		b_right = binary_tree_balance(tree->right) + 1;
 
-	return (b_left + b_right);
+	return (b_left - b_right);
 }
